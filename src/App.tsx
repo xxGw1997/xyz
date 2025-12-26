8d31117a7065434154aead1d4b0dff8eb66ba52e
yangwenxuan <540040588@qq.com>
2025-12-26 23:47:59 +0800

    feat: upload image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
+import { ImageIcon, X } from "lucide-react";
 import ProfileCard from "./components/ProfileCard";
 import iconUrl from "@/assets/iconpattern.png";
 import grainUrl from "@/assets/grain.webp";
-// import LightRays from "./components/LightRays";
+import LightRays from "./components/LightRays";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "./components/ui/button";
 // import Lightning from "./components/Lightning";
 
 function App() {
   const [images, setImages] = useState<string[]>([]);
+
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
   const handleGetImages = async () => {
     const response = await fetch("/api/");
@@ -18,9 +24,24 @@ function App() {
     handleGetImages();
   }, []);
 
+  const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    if (!file.type.startsWith("image/")) {
+      alert("请上传图片文件！");
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreviewUrl(objectUrl);
+
+    () => URL.revokeObjectURL(objectUrl);
+  };
+
   return (
     <div className="w-full h-full flex justify-center items-center relative">
-      {/* <LightRays
+      <LightRays
         raysOrigin="top-center"
         raysColor="#ffffff"
         raysSpeed={1.5}
@@ -31,23 +52,63 @@ function App() {
         noiseAmount={0.1}
         distortion={0.05}
         className="custom-rays"
-      /> */}
+      />
       {/* <Lightning hue={120} xOffset={0} speed={1} intensity={0.5} size={1} /> */}
-      {images.map((url, index) => (
-        <ProfileCard
-          key={index}
-          name=""
-          behindGlowSize="50%"
-          title=""
-          avatarUrl={url}
-          iconUrl={iconUrl}
-          grainUrl={grainUrl}
-          showUserInfo={false}
-          enableTilt={true}
-          enableMobileTilt={true}
-          onContactClick={() => console.log("Contact clicked")}
-        />
-      ))}
+
+      {previewUrl ? (
+        <>
+          <Button
+            className="grid max-w-sm justify-center items-center gap-3 absolute bottom-30"
+            size="icon"
+            variant="ghost"
+            onClick={() => setPreviewUrl(null)}
+          >
+            <X />
+          </Button>
+          <ProfileCard
+            name=""
+            behindGlowSize="50%"
+            title=""
+            avatarUrl={previewUrl}
+            iconUrl={iconUrl}
+            grainUrl={grainUrl}
+            showUserInfo={false}
+            enableTilt={true}
+            enableMobileTilt={true}
+            onContactClick={() => console.log("Contact clicked")}
+          />
+        </>
+      ) : (
+        <>
+          <div className="grid w-full max-w-sm justify-center items-center gap-3 absolute bottom-30">
+            <Label htmlFor="picture">
+              <ImageIcon />
+            </Label>
+            <Input
+              className="hidden"
+              id="picture"
+              accept="image/*"
+              type="file"
+              onChange={handleUpload}
+            />
+          </div>
+          {images.map((url, index) => (
+            <ProfileCard
+              key={index}
+              name=""
+              behindGlowSize="50%"
+              title=""
+              avatarUrl={url}
+              iconUrl={iconUrl}
+              grainUrl={grainUrl}
+              showUserInfo={false}
+              enableTilt={true}
+              enableMobileTilt={true}
+              onContactClick={() => console.log("Contact clicked")}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 }
